a45fa31cdd507a80e10010868b0df442d2de4412
Hafiz Aakif Umar <aakifumar55@gmail.com>
2025-07-24 20:56:19 -0700

    Added font class to navbar

--- grainfree/src/components/layout/Header.tsx
+++ grainfree/src/components/layout/Header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
     return (
         <header className="flex | justify-between | items-center | mr-12 ml-12 pt-5 |">
             {/* Left nav links */}
-            <nav className="flex | flex-row | gap-20 | pt-3 pb-3 pr-20 pl-20 | bg-transparent | text-[#4A4A4A] | border-1 border-[#E6E6E6] | rounded-xl">
+            <nav className="flex | flex-row | gap-20 | pt-3 pb-3 pr-20 pl-20 | bg-transparent | text-[#4A4A4A] font-normal font-[AeonikArabic] | border-1 border-[#E6E6E6] | rounded-xl">
                 <Link href="/system" className="">Get Started</Link>
                 <Link href="/dash" className="">Dashboard</Link>
                 <Link href="/grainhub" className="">GrainFreeHub</Link>
